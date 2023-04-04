2b7cae083ef1db2846e7962e0f4e575421698f3f
HassanShanJava <hassanshan675@gmail.com>
2023-04-04 17:22:12 +0500

    neft detail page

---
+++ src/components/NFT/NFTDetail.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NFTDetail = () => {
+  return (
+    <>
+      <div className="max-h-full min-h-screen w-full  bg-pm-11 px-8">
+        NFTDetail
+
+      </div>
+    </>
+  );
+};
+
+export default NFTDetail;
