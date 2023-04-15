9227c2ab0845a54f3e7458ba8f4d3216d511a895
HassanShanJava <hassanshan675@gmail.com>
2023-04-15 10:56:37 +0500

    resolve errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -29,7 +29,15 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      },
+    ],
   },
+  
+  
 };
 
 module.exports = config;
