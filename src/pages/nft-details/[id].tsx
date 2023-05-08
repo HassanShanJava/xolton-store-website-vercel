94cb95a751712e3eae7055b79f386a9d354048b2
HassanShanJava <hassanshan675@gmail.com>
2023-05-08 12:35:44 +0500

    change api for nft detail page

--- src/pages/nft-details/[id].tsx
+++ src/pages/nft-details/[id].tsx
@@ -9,12 +9,13 @@ const NFTDetails = dynamic(() => import("../../components/NFT/NFTDetail"), {
 
 export async function getStaticPaths() {
   const response: any = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/nft?store_id=${process.env.NEXT_PUBLIC_STORE_ID}`,
+    `${process.env.NEXT_PUBLIC_API_URL}/nft?store_id=${process.env.NEXT_PUBLIC_STORE_ID}&rows=50`,
     {
       headers: {
         "Content-Type": "application/json",
         referer: "xoltanmarketplace.com",
       },
+
     }
   );
   if (!response.ok) {
