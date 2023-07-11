e53bab31f8244916f67e1fc6b05977e7b918c590
HassanShanJava <hassanshan675@gmail.com>
2023-07-11 12:27:48 +0500

    implemented tag manger and changed seo api

--- src/utils/runner.ts
+++ src/utils/runner.ts
@@ -131,10 +131,10 @@ const runSiteAsync = async () => {
   let robotFlag;
   let robotContentFlag;
   let sitemapFlag;
-  if (storeDetail?.seo[0]) {
-    robotFlag = storeDetail?.seo[0]?.robot?.robot
-    robotContentFlag = storeDetail?.seo[0]?.robot?.robot_content
-    sitemapFlag = storeDetail?.seo[0]?.sitemap
+  if (storeDetail?.seo) {
+    robotFlag = storeDetail?.seo?.robot?.robot
+    robotContentFlag = storeDetail?.seo?.robot?.robot_content
+    sitemapFlag = storeDetail?.seo?.sitemap
   }
 
   const sitemapData = `/** @type {import('next-sitemap').IConfig} */
@@ -150,7 +150,7 @@ module.exports = {
     }
 };`
 
-  // fs.writeFileSync("../../next-sitemap.config.js", sitemapData);
+
   console.log(sitemapData, "sitemapData");
   await fs.writeFileSync("next-sitemap.config.js", sitemapData, {
     encoding: "utf8",
@@ -159,9 +159,108 @@ module.exports = {
 };
 
 
+// google tag manager script 
+// sitemapData
+const runDocument = async () => {
+  // find all scripts in subfolder
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/web?&store_id=${process.env.NEXT_PUBLIC_STORE_ID}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        referer: "xoltanmarketplace.com",
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  const result = await response.json();
+
+  // console.log({ result }, "result v")
+  const storeDetail = result?.data;
+  const siteAnalytics = storeDetail?.analytics;
+  const tagmanager = siteAnalytics?.tag_manager?.tag_manager_id;
+  console.log(siteAnalytics, "siteAnalytics")
+
+
+
+  const documentData = `import { useQuery } from "@tanstack/react-query";
+  import { Html, Head, Main, NextScript } from "next/document";
+  
+  import React from "react";
+  
+  import { renderNFTIcon } from "~/utils/helper";
+  
+  export default function Document() {
+    return (
+      <>
+      <Html>
+        <Head>
+          <meta name="description" content={"Store"} />
+          <meta property="og:title" content={"store detail"} key="title" />
+          
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
+            integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&family=Ubuntu:wght@300;400;500;700&display=swap"
+            rel="stylesheet"
+          />
+  
+
+          <script
+          dangerouslySetInnerHTML={{
+            __html: ${"`"+ `
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${tagmanager}');</script>
+            <!-- End Google Tag Manager -->`+"`"},
+          }}
+        />
+        </Head>
+        <body className="font-storeFont">
+          <Main />
+          <NextScript />
+
+          <script
+          dangerouslySetInnerHTML={{
+            __html: ${"`"+ `
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${tagmanager}');</script>
+            <!-- End Google Tag Manager -->`+"`"},
+          }}
+        />
+        </body>
+      </Html>
+      </>
+    );
+  }
+  `;
+
+  console.log(documentData, "documentData")
+  await fs.writeFileSync("src/pages/_document.tsx", documentData, {
+    encoding: "utf8",
+    flag: "w",
+  });
+};
+
 // Self-invocation async function
 (async () => {
-  await Promise.all([runAsync(), runSiteAsync()]);
+  await Promise.all([runAsync(), runSiteAsync(), runDocument()]);
 })().catch((err) => {
   console.error(err);
   throw err;
